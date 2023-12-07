1c891d3df14109db26ec740fbb37eaf8edd79789
mzi1sk <mzilincar@zitec.sk>
2023-12-07 10:45:44 +0100

    datatatable

--- lib/screens/order/order_page.dart
+++ lib/screens/order/order_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../idempiere_rest/po.dart';
 import '../../utilities/po_lead.dart';
 import '../../view_models/main_view_models.dart';
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/material.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({
@@ -41,4 +44,48 @@ class OrderPageState extends  State<OrderPage>    {
       ),
     );
   }
+
+
+}
+
+class UserDataTableSource extends DataTableSource {
+  UserDataTableSource({
+    required List<Po> userData,
+  })  : _userData = userData,
+        assert(userData != null);
+
+  final List<Po> _userData;
+
+  @override
+  DataRow getRow(int index) {
+    assert(index >= 0);
+
+    if (index >= _userData.length) {
+      return null;
+    }
+    final _user = _userData[index];
+
+    return DataRow.byIndex(
+      index: index, // DON'T MISS THIS
+      cells: <DataCell>[
+        DataCell(Text('${_user.json_po['Name']}')),
+        DataCell(Text('${_user.json_po['Value']}')),
+        DataCell(Text('${_user.json_po['EMail']}')),
+        DataCell(Text('${_user.json_po['Phone']}')),
+        DataCell(Text('${_user.json_po['Created']}')),
+        DataCell(Text('${_user.json_po['IsVendorLead']}')),
+        DataCell(Text('${_user.json_po['IsSalesLead']}')),
+      ],
+    );
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => _userData.length;
+
+  @override
+  int get selectedRowCount => 0;
+
 }
