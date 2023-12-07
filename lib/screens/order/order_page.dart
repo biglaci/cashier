473c532243d46b62159b895d887db98a9cbcfaa6
MBpro\laci <ladislav.nagy@gmail.com>
2023-12-07 10:45:17 +0100

    datatables2

--- lib/screens/order/order_page.dart
+++ lib/screens/order/order_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../idempiere_rest/po.dart';
 import '../../utilities/po_lead.dart';
 import '../../view_models/main_view_models.dart';
+import 'package:data_table_2/data_table_2.dart';
+
 
 class OrderPage extends StatefulWidget {
   const OrderPage({
@@ -31,11 +34,55 @@ class OrderPageState extends  State<OrderPage>    {
                   child: Text('Loading...'),
                 );
               } else {
-                return Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  runSpacing: 4.0,
-                  children: snapshot.data,
-                );
+                return
+                  SingleChildScrollView(
+                    // Using scrollView for scrolling and formating
+                      scrollDirection: Axis.vertical,
+                      // using FittedBox for fitting complte table in screen horizontally.
+                      child: FittedBox(
+                          child: DataTable(
+                            sortColumnIndex: 1,
+                            showCheckboxColumn: false,
+                            border: TableBorder.all(width: 1.0),
+                            // Data columns as required by APIs data.
+                            columns: const [
+                              DataColumn(
+                                  label: Text(
+                                    "id",
+                                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                                  )),
+                              DataColumn(
+                                  label: Text(
+                                    "name ",
+                                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                                  )),
+                              DataColumn(
+                                  label: Text(
+                                    "value",
+                                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                                  )),
+                            ],
+                            // Main logic and code for geting data and shoing it in table rows.
+                            rows: snapshot.data
+                                .map(
+                              //maping each rows with datalist data
+                                    (data) => DataRow(cells: [
+                                  DataCell(
+                                    Text(data.id.toString(),
+                                        style: const TextStyle(
+                                            fontSize: 25, fontWeight: FontWeight.w500)),
+                                  ),
+                                  DataCell(Text(data.json_po['Name'].toString(),
+                                      style: const TextStyle(
+                                          fontSize: 26, fontWeight: FontWeight.w500))),
+                                  DataCell(
+                                    Text(data.json_po['Value'].toString(),
+                                        style: const TextStyle(
+                                            fontSize: 26, fontWeight: FontWeight.w500)),
+                                  ),
+                                ]))
+                                .toList(), // converting at last into list.
+                          )));
               }
             }),
       ),
