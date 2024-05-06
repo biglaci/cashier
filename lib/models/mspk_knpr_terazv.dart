66d97f7fba6ca963cdb602f7a92e21361631d5c0
MBpro\laci <ladislav.nagy@gmail.com>
2024-05-06 18:06:15 +0200

    nove triedy k dashboard

--- lib/models/mspk_knpr_terazv.dart
+++ lib/models/mspk_knpr_terazv.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 
-import 'spk_knpr_nastenka.dart';
+import 'spk_knpr_terazv.dart';
 
 import 'package:flutter/widgets.dart';
 
 import '../idempiere_rest/idempiere_client.dart';
 
-class MspkKnprNastenka extends ChangeNotifier {
-  MspkKnprNastenka({
+class MspkKnprTerazv extends ChangeNotifier {
+  MspkKnprTerazv({
     required this.columns,
     required this.model,
     required this.orderBy,
@@ -24,21 +24,21 @@ class MspkKnprNastenka extends ChangeNotifier {
   final List<String> orderBy;
   final int top;
   final int skip;
-  SpkKnprNastenka? data;
+  SpkKnprTerazv? data;
 
   List<DataColumn> _columnList = [];
   List<DataColumn> get columnList => _columnList;
   Future getData() async {
 
     var mJson = await IdempiereClient().getJson(
-        'models/spk_knpr_nastenka',
+        'models/spk_knpr_terazv',
         orderBy: ['Created'],
-        select: ['AD_Org_ID', 'Created','Value', 'Name', 'Description'],
+        select: [],
         top: 20,
         skip: 0,
         showsql: true);
     // now we have a json...
-    this.data = SpkKnprNastenka.fromJson(mJson);
+    this.data = SpkKnprTerazv.fromJson(mJson);
     this.notifyListeners(); // for callback to view
   }
 
--- lib/models/spk_knpr_terazv.dart
+++ lib/models/spk_knpr_terazv.dart
@@ -1,4 +1,4 @@
-class SpkKnprNastenka {
+class SpkKnprTerazv {
   int? pageCount;
   int? recordsSize;
   int? skipRecords;
@@ -6,7 +6,7 @@ class SpkKnprNastenka {
   int? arrayCount;
   List<Records>? records;
 
-  SpkKnprNastenka(
+  SpkKnprTerazv(
       {this.pageCount,
         this.recordsSize,
         this.skipRecords,
@@ -14,7 +14,7 @@ class SpkKnprNastenka {
         this.arrayCount,
         this.records});
 
-  SpkKnprNastenka.fromJson(Map<String, dynamic> json) {
+  SpkKnprTerazv.fromJson(Map<String, dynamic> json) {
     pageCount = json['page-count'];
     recordsSize = json['records-size'];
     skipRecords = json['skip-records'];
@@ -45,54 +45,80 @@ class SpkKnprNastenka {
 class Records {
   int? id;
   String? uid;
-  ReferenceID? aDClientID;
-  ReferenceID? aDOrgID;
+ ReferenceID? aDOrgID;
+ ReferenceID? aDClientID;
   String? created;
-  ReferenceID? createdBy;
-  String? description;
+ ReferenceID? createdBy;
+  String? updated;
+ ReferenceID? updatedBy;
   bool? isActive;
   String? name;
-  String? updated;
-  ReferenceID? updatedBy;
-  String? value;
+  String? spkKnprLokalitytxt;
+ ReferenceID? spkKnprUcelID;
+  String? spkKnpnDatum;
+  String? spkKnpnDatumPlan;
+  int? spkPocHos;
+  String? spkKnprHostxt;
+  int? spkPocPoz;
+  String? spkKnprPoztxt;
+  int? spkPocUlo;
+  String? spkKnprUlotxt;
   String? modelName;
 
   Records(
       {this.id,
         this.uid,
-        this.aDClientID,
         this.aDOrgID,
+        this.aDClientID,
         this.created,
         this.createdBy,
-        this.description,
-        this.isActive,
-        this.name,
         this.updated,
         this.updatedBy,
-        this.value,
+        this.isActive,
+        this.name,
+        this.spkKnprLokalitytxt,
+        this.spkKnprUcelID,
+        this.spkKnpnDatum,
+        this.spkKnpnDatumPlan,
+        this.spkPocHos,
+        this.spkKnprHostxt,
+        this.spkPocPoz,
+        this.spkKnprPoztxt,
+        this.spkPocUlo,
+        this.spkKnprUlotxt,
         this.modelName});
 
   Records.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     uid = json['uid'];
-    aDClientID = json['AD_Client_ID'] != null
-        ? new ReferenceID.fromJson(json['AD_Client_ID'])
-        : null;
     aDOrgID = json['AD_Org_ID'] != null
         ? new ReferenceID.fromJson(json['AD_Org_ID'])
+        : null;
+    aDClientID = json['AD_Client_ID'] != null
+        ? new ReferenceID.fromJson(json['AD_Client_ID'])
         : null;
     created = json['Created'];
     createdBy = json['CreatedBy'] != null
         ? new ReferenceID.fromJson(json['CreatedBy'])
         : null;
-    description = json['Description'];
-    isActive = json['IsActive'];
-    name = json['Name'];
     updated = json['Updated'];
     updatedBy = json['UpdatedBy'] != null
         ? new ReferenceID.fromJson(json['UpdatedBy'])
         : null;
-    value = json['Value'];
+    isActive = json['IsActive'];
+    name = json['Name'];
+    spkKnprLokalitytxt = json['spk_knpr_lokalitytxt'];
+    spkKnprUcelID = json['spk_knpr_ucel_ID'] != null
+        ? new ReferenceID.fromJson(json['spk_knpr_ucel_ID'])
+        : null;
+    spkKnpnDatum = json['spk_knpn_datum'];
+    spkKnpnDatumPlan = json['spk_knpn_datum_plan'];
+    spkPocHos = json['spk_poc_hos'];
+    spkKnprHostxt = json['spk_knpr_hostxt'];
+    spkPocPoz = json['spk_poc_poz'];
+    spkKnprPoztxt = json['spk_knpr_poztxt'];
+    spkPocUlo = json['spk_poc_ulo'];
+    spkKnprUlotxt = json['spk_knpr_ulotxt'];
     modelName = json['model-name'];
   }
 
@@ -100,24 +126,34 @@ class Records {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
     data['uid'] = this.uid;
-    if (this.aDClientID != null) {
-      data['AD_Client_ID'] = this.aDClientID!.toJson();
-    }
     if (this.aDOrgID != null) {
       data['AD_Org_ID'] = this.aDOrgID!.toJson();
+    }
+    if (this.aDClientID != null) {
+      data['AD_Client_ID'] = this.aDClientID!.toJson();
     }
     data['Created'] = this.created;
     if (this.createdBy != null) {
       data['CreatedBy'] = this.createdBy!.toJson();
     }
-    data['Description'] = this.description;
-    data['IsActive'] = this.isActive;
-    data['Name'] = this.name;
     data['Updated'] = this.updated;
     if (this.updatedBy != null) {
       data['UpdatedBy'] = this.updatedBy!.toJson();
     }
-    data['Value'] = this.value;
+    data['IsActive'] = this.isActive;
+    data['Name'] = this.name;
+    data['spk_knpr_lokalitytxt'] = this.spkKnprLokalitytxt;
+    if (this.spkKnprUcelID != null) {
+      data['spk_knpr_ucel_ID'] = this.spkKnprUcelID!.toJson();
+    }
+    data['spk_knpn_datum'] = this.spkKnpnDatum;
+    data['spk_knpn_datum_plan'] = this.spkKnpnDatumPlan;
+    data['spk_poc_hos'] = this.spkPocHos;
+    data['spk_knpr_hostxt'] = this.spkKnprHostxt;
+    data['spk_poc_poz'] = this.spkPocPoz;
+    data['spk_knpr_poztxt'] = this.spkKnprPoztxt;
+    data['spk_poc_ulo'] = this.spkPocUlo;
+    data['spk_knpr_ulotxt'] = this.spkKnprUlotxt;
     data['model-name'] = this.modelName;
     return data;
   }
